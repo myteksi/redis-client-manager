5400e10bf9cc76967b369f79cab6b169a36dc404
fadrizul <fadrizul@gmail.com>
2014-04-11 16:59:12 +0800

    (IMPROVE) Implement semi-colon and function scoping and graceful exits

--- index.js
+++ index.js
@@ -2,26 +2,41 @@
 Copyright (c) MyTeksi GrabTaxi 2013
 Author: Fadrizul Hasani <fadrizul@gmail.com>
 */
+(function () {
+  var redis, redisClient;
 
-var redis = require('redis')
-var redisClient = null
+  redis = require('redis');
+  redisClient = null;
 
-// When main process dies, kill the redis connection as well
-process.on('exit', function () {
-  // Check if the connection exist before quitting
-  if (redisClient != null) {
-    redisClient.quit()
+  function gracefulExit(options, err) {
+    if (options.cleanup) {
+      // Check if the connection exist before quitting
+      if (redisClient != null) {
+        redisClient.quit();
+      }
+    }
+
+    if (err) {
+      console.log(err.stack);
+    }
+
+    if (options.exit) {
+      redisClient.quit();
+      process.exit();
+    }
   }
-})
 
-// Function to get the connection, if the connection exist returns the connection.
-// If it doesn't exist, create a new connection
-// Take in options for the port & host
-exports.getClient = function (opts) {
-  var opts = opts || {}
+  // Handle processes signals
+  process.on('exit', gracefulExit.bind(null, { cleanup : true }));
+  process.on('SIGINT', gracefulExit.bind(null, { exit : true }));
+  process.on('uncaughtException', gracefulExit.bind(null, { exit : true }));
 
-  if (redisClient === null) {
-    redisClient = redis.createClient(opts.redisPort, opts.redisHost)
-  }
-  return redisClient
-}
+  // Function to get the connection, if the connection exist returns the connection.
+  // If it doesn't exist, create a new connection
+  // Take in options for the port & host
+  exports.getClient = function (opts) {
+    var opts = opts || {};
+    return redisClient || redis.createClient(opts.redisPort, opts.redisHost);
+  };
+
+}).call(this);

e8a5083182c2b87b14baa2877be1c8e22185b0e8
fadrizul <fadrizul@gmail.com>
2014-04-11 17:11:09 +0800

    (IMPROVE) Implement semicolons and function scoping

--- specs/redis.client.manager.spec.js
+++ specs/redis.client.manager.spec.js
@@ -2,28 +2,31 @@
 Copyright (c) MyTeksi GrabTaxi 2013
 Author: Fadrizul Hasani <fadrizul@gmail.com>
 */
+(function () {
+  var should, redisManager;
 
-var should = require('should')
-var redisManager = require('../index')
+  should = require('should');
+  redisManager = require('../index');
 
-describe('When call the manager to get the redis client', function () {
-  beforeEach(function (done) {
-    redisClient = redisManager.getClient()
-    done()
+  describe('When call the manager to get the redis client', function () {
+    before(function (done) {
+      redisClient = redisManager.getClient()
+      done()
+    })
+
+    after(function (done) {
+      redisClient.quit()
+      done()
+    })
+
+    it('should not return an empty object', function (done) {
+      should.exist(redisClient)
+      done()
+    })
+
+    it('should be in ready state', function (done) {
+      redisClient.ready.should.be.true
+      done()
+    })
   })
-
-  after(function (done) {
-    redisClient.quit()
-    done()
-  })
-
-  it('should not return an empty object', function (done) {
-    should.exist(redisClient)
-    done()
-  })
-
-  it('should be in ready state', function (done) {
-    redisClient.ready.should.be.true
-    done()
-  })
-})
+}).call(this);

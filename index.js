4148ebac0354ed7582caa2331dafe34742a9f8cc
Hao Wooi Lim <zybler@gmail.com>
2014-09-04 17:14:40 +0800

    Refactor code to allow for an optional second client connecection
    Summary: Allow for a second client.
    
    Test Plan: npm test
    
    Reviewers: fadrizul
    
    Subscribers: fadrizul
    
    Differential Revision: http://diff.myteksi.net/D25

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 /*
-Copyright (c) MyTeksi GrabTaxi 2013
-Author: Fadrizul Hasani <fadrizul@gmail.com>
-*/
-(function () {
-  var redis, redisClient;
+ * Copyright (c) MyTeksi GrabTaxi 2013
+ * Author: Fadrizul Hasani <fadrizul@gmail.com>
+ */
+(function () { 'use strict';
 
-  redis = require('redis');
-  redisClient = null;
+  var redis, redisClient, redisSecondClient;
 
-  function gracefulExit(options, err) {
-    if (options.cleanup) {
+  redis             = require('redis');
+  redisClient       = null;
+  redisSecondClient = null;
+
+  function gracefulExit (opts, err) {
+    if (opts.cleanup) {
       // Check if the connection exist before quitting
       if (redisClient != null) {
         redisClient.quit();
+        redisClient = null;
+      }
+
+      if (redisSecondClient != null) {
+        redisSecondClient.quit();
+        redisSecondClient = null;
       }
     }
 
@@ -20,7 +28,7 @@ Author: Fadrizul Hasani <fadrizul@gmail.com>
       console.log(err.stack);
     }
 
-    if (options.exit) {
+    if (opts.exit) {
       process.exit();
     }
   }
@@ -30,15 +38,28 @@ Author: Fadrizul Hasani <fadrizul@gmail.com>
   process.on('SIGINT', gracefulExit.bind(null, { exit : true }));
   process.on('uncaughtException', gracefulExit.bind(null, { exit : true }));
 
-  // Function to get the connection, if the connection exist returns the connection.
-  // If it doesn't exist, create a new connection
-  // Take in options for the port & host
+  // Function to get the connection, if the connection exist returns the
+  // connection.
+  // If it doesn't exist, create a new connection.
+  // Take in options for the port & host.
   exports.getClient = function (opts) {
-    var opts = opts || {};
+    opts = opts || {};
+
     if (redisClient === null) {
       redisClient = redis.createClient(opts.redisPort, opts.redisHost);
     }
+
     return redisClient;
+  };
+
+  exports.getSecondClient = function (opts) {
+    opts = opts || {};
+
+    if (redisSecondClient === null) {
+      redisSecondClient = redis.createClient(opts.redisPort, opts.redisHost);
+    }
+
+    return redisSecondClient;
   };
 
 }).call(this);
